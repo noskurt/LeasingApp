98c640cf44425255dfc36834ee260d5e77636cb6
Emre Bozkurt <emre15emre@hotmail.com>
2023-09-24 08:02:36 +0200

    Dockerfile and init script created for run app in docker

---
+++ scripts/init.sh
@@ -0,0 +1,38 @@
+#! /bin/bash
+
+set -eo pipefail
+
+printHelp() {
+    echo "This script is for running the Spring Application in the Docker container."
+    echo ""
+    echo "Options:"
+    echo "  -p    It's for the Spring Active Profile. If you want initial data, you need to pass 'load-data'"
+    echo "  -h    Print this"
+}
+
+PROFILE="default"
+
+while :; do
+    case $1 in
+    -p | --profile)
+        PROFILE=$2
+        shift
+        ;;
+    -h | --help)
+        printHelp
+        exit 0
+        ;;
+    -?*)
+        echo "Error: Unknown option: $1"
+        printHelp
+        exit 0
+        ;;
+    *)
+        break
+        ;;
+    esac
+    shift
+done
+
+cd .. && ./gradlew bootBuildImage --imageName=leasing/app
+cd ./docker && PROFILE="$PROFILE" docker-compose up
