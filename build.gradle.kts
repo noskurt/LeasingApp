3b5e7f18b1908243f28176259db3484d40a14c23
Emre Bozkurt <emre15emre@hotmail.com>
2024-01-16 14:37:05 +0100

    update versions

--- build.gradle.kts
+++ build.gradle.kts
@@ -2,8 +2,8 @@ import org.gradle.api.tasks.testing.logging.TestExceptionFormat
 
 plugins {
 	java
-	id("org.springframework.boot") version "3.1.4"
-	id("io.spring.dependency-management") version "1.1.3"
+	id("org.springframework.boot") version "3.2.1"
+	id("io.spring.dependency-management") version "1.1.4"
 }
 
 group = "leasing"
@@ -28,8 +28,8 @@ dependencies {
 	implementation("org.springframework.boot:spring-boot-starter-web")
     implementation("org.springframework.boot:spring-boot-starter-validation")
     implementation("org.flywaydb:flyway-core")
-	implementation("org.springdoc:springdoc-openapi-starter-webmvc-ui:2.2.0")
-    implementation("net.datafaker:datafaker:2.0.1")
+	implementation("org.springdoc:springdoc-openapi-starter-webmvc-ui:2.3.0")
+    implementation("net.datafaker:datafaker:2.1.0")
     implementation("org.mapstruct:mapstruct:1.5.5.Final")
     compileOnly("org.projectlombok:lombok")
 	runtimeOnly("org.postgresql:postgresql")
